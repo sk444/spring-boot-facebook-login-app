d0d26241c4233dd6d8818ed93cec92e9b6a9909c
shubham kumar <shubhamkumar1980@gmail.com>
2020-09-09 15:36:35 +0530

    initial commit

---
+++ src/main/java/com/pixeltrice/springbootfacebookloginapp/SpringBootFacebookLoginAppApplication.java
@@ -0,0 +1,13 @@
+package com.pixeltrice.springbootfacebookloginapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFacebookLoginAppApplication {
+	
+  
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFacebookLoginAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pixeltrice/springbootfacebookloginapp/SpringBootFacebookLoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixeltrice.springbootfacebookloginapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFacebookLoginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
